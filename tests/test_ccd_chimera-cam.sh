288366adb232c501b6bf68f22d2ae94436156cc6
William Schoenell <wschoenell@carnegiescience.edu>
2025-12-09 10:55:41 -0800

    tests executed on swope last night

--- tests/test_ccd_chimera-cam.sh
+++ tests/test_ccd_chimera-cam.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 set -xe
-chimera-cam --config ../etc/chimera_ccd.config -v --version
-chimera-cam --config ../etc/chimera_ccd.config -v --expose -n 2 -t 1.2 -i 3 -o './$DATE-$TIME.fits' -f Rc1528 --shutter=open --force-display
-chimera-cam --config ../etc/chimera_ccd.config -v --expose --shutter=close --object=Test
-# chimera-cam --config ../etc/chimera_ccd.config -v --expose --shutter=leave --binning=BINNING 2x2 
-chimera-cam --config ../etc/chimera_ccd.config -v --expose --compress=gzip
-chimera-cam --config ../etc/chimera_ccd.config -v --expose --compress=zip
-chimera-cam --config ../etc/chimera_ccd.config -v --expose --compress=bz2
-chimera-cam --config ../etc/chimera_ccd.config -v --expose --compress=fits_rice --ignore-dome
-chimera-cam --config ../etc/chimera_ccd.config -v --expose --force-display
-chimera-cam --config ../etc/chimera_ccd.config -v -F
-chimera-cam --config ../etc/chimera_ccd.config -v --info
-chimera-cam --config ../etc/chimera_ccd.config -v --expose --bias
-chimera-cam --config ../etc/chimera_ccd.config -v --expose --flat
-chimera-cam --config ../etc/chimera_ccd.config -v --expose --sky-flat
-chimera-cam --config ../etc/chimera_ccd.config -v --expose --dark -t2
+chimera-cam -v --version
+chimera-cam -v --expose -n 2 -t 1.2 -i 3 -o './$DATE-$TIME.fits' -f Rc1528 --shutter=open --force-display
+chimera-cam -v --expose --shutter=close --object=Test
+# chimera-cam -v --expose --shutter=leave --binning=BINNING 2x2 
+chimera-cam -v --expose --compress=gzip
+chimera-cam -v --expose --compress=zip
+chimera-cam -v --expose --compress=bz2
+chimera-cam -v --expose --compress=fits_rice --ignore-dome
+chimera-cam -v --expose --force-display
+chimera-cam -v -F
+chimera-cam -v --info
+chimera-cam -v --expose --bias
+chimera-cam -v --expose --flat
+chimera-cam -v --expose --sky-flat
+chimera-cam -v --expose --dark -t2
 
 # # open dome slit, slew telescope, take an exposure
 # chimera-dome --open-slit --track && \
 # chimera-tel --slew --az 10 --alt 60 && \
-# chimera-cam --config ../etc/chimera_ccd.config -v -w --expose 
+# chimera-cam -v -w --expose 
