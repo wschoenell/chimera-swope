116ca14b601d9714607741c1adc2581956dda8f4
William Schoenell <wschoenell@carnegiescience.edu>
2025-12-09 10:55:02 -0800

    rotator: update callbacks to match ife

--- chimera_swope/instruments/swoperotator.py
+++ chimera_swope/instruments/swoperotator.py
@@ -13,11 +13,11 @@ class SwopeRotator(RotatorBase):
         return self._pos
 
     def move_to(self, angle):
-        self.moving_started()
+        self.move_begin()
         self._pos = angle
-        self.moving_complete()
+        self.move_complete()
 
     def move_by(self, offset):
-        self.moving_started()
+        self.move_begin()
         self._pos += offset  # todo: wrap around 360, make sure within limits, use actual hardware
-        self.moving_complete()
+        self.move_complete()
